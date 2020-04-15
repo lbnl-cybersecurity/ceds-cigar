dd5cf22561255a864e699bb87c4858e683ffb871
milesial <milesial@users.noreply.github.com>
2020-04-15 03:50:08 -0700

    Alex script: handle multiple tracked inverters + unbalance plot
    Doesn't work with tensorboard yet because of nested dicts

--- rl/notebooks/understand_hyperparameters.py
+++ rl/notebooks/understand_hyperparameters.py
@@ -33,6 +33,7 @@ def parse_cli_args():
                         help='number of evaluation rounds to run to smooth random results')
     parser.add_argument("--algo", help="use PPO or APPO", choices=['ppo', 'appo'],
                         nargs='?', const='ppo', default='ppo', type=str.lower)
+    parser.add_argument('--unbalance', dest='unbalance', action='store_true')
 
     return parser.parse_args()
 
@@ -79,7 +80,7 @@ def on_episode_start(info):
 def on_episode_step(info):
     episode = info["episode"]
     action = episode.last_action_for()
-    if action != episode.user_data["true_actions"][-1].action:
+    if (action != episode.user_data["true_actions"][-1].action).all():
         episode.user_data["true_actions"].append(ActionTuple(action, episode.length))
 
     if episode.last_info_for() is not None:
@@ -101,8 +102,8 @@ def on_episode_end(info):
 
     env = info['env'].vector_env.envs[0]
     t_id = env.unwrapped.tracking_ids[0]
-    episode.hist_data.update(env.unwrapped.tracking_infos[t_id])
-    episode.hist_data['adversary_a_val'] = env.unwrapped.tracking_infos['adversary_' + t_id]['a_val']
+    episode.hist_data['tracking'] = env.unwrapped.tracking_infos
+    # episode.hist_data['adversary_a_val'] = env.unwrapped.tracking_infos['adversary_' + t_id]['a_val']
     hack_start = int([k for k, v in env.unwrapped.k.scenario.hack_start_times.items() if 'adversary_' + t_id in v][0])
     hack_end = int([k for k, v in env.unwrapped.k.scenario.hack_end_times.items() if 'adversary_' + t_id in v][0])
     episode.custom_metrics["hack_start"] = hack_start
@@ -118,31 +119,63 @@ def get_translation_and_slope(a_val):
     return translation, slope
 
 
-def plot_episode(ep, epoch=None):
+def plot_episode(ep, epoch=None, unbalanced=True):
     plt.rc('font', size=15)
     plt.rc('figure', titlesize=35)
-    f, ax = plt.subplots(5, figsize=(25, 20))
-    title = '[epoch {}] total reward: {:.2f}'.format(epoch, ep.episode_reward)
-    f.suptitle(title)
-    ax[0].plot(ep.hist_data['v_val'], color='tab:blue', label='voltage')
+    if not unbalanced:
+        k = list(ep.hist_data['tracking'].keys())[0]
+        data = ep.hist_data['tracking'][k]
+        f, ax = plt.subplots(5, figsize=(25, 20))
+        title = '[epoch {}] total reward: {:.2f}'.format(epoch, ep.episode_reward)
+        f.suptitle(title)
+        ax[0].plot(data['v_val'], color='tab:blue', label='voltage')
 
-    ax[1].plot(ep.hist_data['y_val'], color='tab:blue', label='oscillation observer')
+        ax[1].plot(data['y_val'], color='tab:blue', label='oscillation observer')
 
-    ax[2].plot(ep.hist_data['q_set'], color='tab:blue', label='q_set')
-    ax[2].plot(ep.hist_data['q_val'], color='tab:orange', label='q_val')
+        ax[2].plot(data['q_set'], color='tab:blue', label='q_set')
+        ax[2].plot(data['q_val'], color='tab:orange', label='q_val')
 
-    translation, slope = get_translation_and_slope(ep.hist_data['a_val'])
-    ax[3].plot(translation, color='tab:blue', label='RL translation')
-    ax[3].plot(slope, color='tab:purple', label='RL slope (a2-a1)')
+        translation, slope = get_translation_and_slope(data['a_val'])
+        ax[3].plot(translation, color='tab:blue', label='RL translation')
+        ax[3].plot(slope, color='tab:purple', label='RL slope (a2-a1)')
 
-    translation, slope = get_translation_and_slope(ep.hist_data['adversary_a_val'])
-    ax[4].plot(translation, color='tab:orange', label='hacked translation')
-    ax[4].plot(slope, color='tab:red', label='hacked slope (a2-a1)')
-    ax[0].set_ylim([0.93, 1.07])
-    ax[1].set_ylim([0, 0.8])
-    ax[2].set_ylim([-280, 280])
-    ax[3].set_ylim([-0.055, 0.055])
-    ax[4].set_ylim([-0.055, 0.055])
+        translation, slope = get_translation_and_slope(data['adversary_a_val'])
+        ax[4].plot(translation, color='tab:orange', label='hacked translation')
+        ax[4].plot(slope, color='tab:red', label='hacked slope (a2-a1)')
+        ax[0].set_ylim([0.93, 1.07])
+        ax[1].set_ylim([0, 0.8])
+        ax[2].set_ylim([-280, 280])
+        ax[3].set_ylim([-0.06, 0.06])
+        ax[4].set_ylim([-0.06, 0.06])
+    else:
+        keys = [k for k in ep.hist_data['tracking'].keys() if 'adversary' not in k and 'inv' in k]
+        keys_all = list(ep.hist_data['tracking'].keys())
+        reg = False
+        for k in keys_all:
+            if 'reg' in k:
+                reg = k
+                break
+
+        data = ep.hist_data['tracking']
+        f, ax = plt.subplots(2 + len(keys) + (reg is not False), figsize=(25, 8 + 4 * len(keys) + 4 * (reg is not False)))
+        title = '[epoch {}] total reward: {:.2f}'.format(epoch, ep.episode_reward)
+        f.suptitle(title)
+        for i, k in enumerate(keys):
+            ax[0].plot(data[k]['v_val'], label='voltage ({})'.format(k))
+            ax[1].plot(data[k]['y_val'], label='unbalance observer ({})'.format(k))
+
+            translation, slope = get_translation_and_slope(data[k]['a_val'])
+            ax[2+i].plot(translation, label='RL translation ({})'.format(k))
+            ax[2+i].plot(slope, label='RL slope (a2-a1) ({})'.format(k))
+
+        if reg:
+            ax[-1].plot(data[reg]['reg_val'], label=reg)
+
+        ax[0].set_ylim([0.90, 1.10])
+        ax[1].set_ylim([0, 0.1])
+        ax[2].set_ylim([-280, 280])
+        for i in range(len(keys)):
+            ax[2+i].set_ylim([-0.06, 0.06])
 
     for a in ax:
         a.grid(b=True, which='both')
@@ -160,19 +193,21 @@ def save_best_policy(trainer, episodes):
         trainer.global_vars['best_eval_reward'] = mean_r
         # save policy
         shutil.rmtree(os.path.join(trainer.global_vars['reporter_dir'], 'best', 'policy'), ignore_errors=True)
-        trainer.get_policy().export_model(os.path.join(trainer.global_vars['reporter_dir'], 'best', 'policy'))
+#        trainer.get_policy().export_model(os.path.join(trainer.global_vars['reporter_dir'], 'best', 'policy'))
         # save plots
         ep = episodes[-1]
         f = plot_episode(ep, trainer.iteration)
         f.savefig(os.path.join(trainer.global_vars['reporter_dir'], 'best', 'eval.png'))
 
         # save CSV
-        ep_hist = pd.DataFrame(dict(v=ep.hist_data['v_val'], y=ep.hist_data['y_val'],
-                                    q_set=ep.hist_data['q_set'], q_val=ep.hist_data['q_val']))
-        a_hist = pd.DataFrame(ep.hist_data['a_val'], columns=['a1', 'a2', 'a3', 'a4', 'a5'])
-        adv_a_hist = pd.DataFrame(ep.hist_data['a_val'], columns=['adv_a1', 'adv_a2', 'adv_a3', 'adv_a4', 'adv_a5'])
-        translation, slope = get_translation_and_slope(ep.hist_data['a_val'])
-        adv_translation, adv_slope = get_translation_and_slope(ep.hist_data['adversary_a_val'])
+        data = ep.hist_data['tracking']
+        k = list(data.keys())[0]
+        ep_hist = pd.DataFrame(dict(v=data[k]['v_val'], y=data[k]['y_val'],
+                                    q_set=data[k]['q_set'], q_val=data[k]['q_val']))
+        a_hist = pd.DataFrame(data[k]['a_val'], columns=['a1', 'a2', 'a3', 'a4', 'a5'])
+        adv_a_hist = pd.DataFrame(data[k]['a_val'], columns=['adv_a1', 'adv_a2', 'adv_a3', 'adv_a4', 'adv_a5'])
+        translation, slope = get_translation_and_slope(data[k]['a_val'])
+        adv_translation, adv_slope = get_translation_and_slope(data['adversary_'+k]['a_val'])
         trans_slope_hist = pd.DataFrame(dict(translation=translation, slope=slope,
                                              adv_translation=adv_translation, adv_slope=adv_slope))
 
@@ -200,6 +235,8 @@ def run_train(config, reporter):
 
     # needed so that the custom eval fn knows where to save plots
     trainer.global_vars['reporter_dir'] = reporter.logdir
+    trainer.global_vars['unbalance'] = config['unbalance']
+
     for _ in tqdm(range(config['epochs'])):
         results = trainer.train()
         reporter(**results)
@@ -220,78 +257,87 @@ def run_hp_experiment(full_config, name):
         pickle.dump(res.trial_dataframes, f)
 
 
-pycigar_params = {'exp_tag': 'cooperative_multiagent_ppo',
-                  'env_name': 'CentralControlPVInverterContinuousEnv',
-                  'simulator': 'opendss',
-                  'tracking_ids': ['inverter_s701a', 'adversary_inverter_s701a']}
-
-create_env, env_name = make_create_env(pycigar_params, version=0)
-register_env(env_name, create_env)
-
-sim_params = input_parser('ieee37busdata')
-base_config = {
-    "env": env_name,
-    "gamma": 0.5,
-    'lr': 2e-4,
-    'env_config': deepcopy(sim_params),
-    'sample_batch_size': 50,
-    'train_batch_size': 500,
-    'clip_param': 0.1,
-    'lambda': 0.95,
-    'vf_clip_param': 100,
-
-    'num_workers': 3,
-    'num_cpus_per_worker': 1,
-    'num_cpus_for_driver': 1,
-    'num_envs_per_worker': 1,
-
-    'log_level': 'WARNING',
-
-    'model': {
-        'fcnet_activation': 'tanh',
-        'fcnet_hiddens': [128, 64, 32],
-        'free_log_std': False,
-        'vf_share_layers': True,
-        'use_lstm': False,
-        'state_shape': None,
-        'framestack': False,
-        'zero_mean': True,
-    },
-
-    # ==== EXPLORATION ====
-    'explore': True,
-    'exploration_config': {
-        'type': 'StochasticSampling',  # default for PPO
-    },
-
-    # ==== EVALUATION ====
-    "evaluation_num_workers": 1,
-    'evaluation_num_episodes': 2,
-    "evaluation_interval": 5,
-    "custom_eval_function": tune.function(custom_eval_function),
-    'evaluation_config': {
-        "seed": 42,
-        # IMPORTANT NOTE: For policy gradients, this might not be the optimal policy
-        'explore': False,
-        'env_config': deepcopy(sim_params),
-    },
-
-    # ==== CUSTOM METRICS ====
-    "callbacks": {
-        "on_episode_start": tune.function(on_episode_start),
-        "on_episode_step": tune.function(on_episode_step),
-        "on_episode_end": tune.function(on_episode_end),
-    },
-}
-# eval environment should not be random across workers
-eval_start = 100  # random.randint(0, 3599 - 500)
-base_config['evaluation_config']['env_config']['scenario_config']['start_end_time'] = [eval_start, eval_start + 750]
-del base_config['evaluation_config']['env_config']['attack_randomization']
-base_config['evaluation_config']['env_config']['scenario_config']['multi_config'] = False
-
 if __name__ == '__main__':
-    ray.init(local_mode=False)
     args = parse_cli_args()
+    args.unbalance = True
+
+    if args.unbalance:
+        pycigar_params = {'exp_tag': 'cooperative_multiagent_ppo',
+                          'env_name': 'CentralControlPVInverterContinuousEnv',
+                          'simulator': 'opendss',
+                          'tracking_ids': ['inverter_s701a', 'adversary_inverter_s701a', 'reg1']}
+    else:
+        pycigar_params = {'exp_tag': 'cooperative_multiagent_ppo',
+                          'env_name': 'CentralControlPVInverterEnv',
+                          'simulator': 'opendss',
+                          'tracking_ids': ['inverter_s701a', 'adversary_inverter_s701a']}
+
+    create_env, env_name = make_create_env(pycigar_params, version=0)
+    register_env(env_name, create_env)
+
+    sim_params = input_parser('ieee37busdata_regulator_attack' if args.unbalance else 'ieee37busdata')
+
+    base_config = {
+        "env": env_name,
+        "gamma": 0.5,
+        'lr': 2e-4,
+        'env_config': deepcopy(sim_params),
+        'sample_batch_size': 50,
+        'train_batch_size': 500,
+        'clip_param': 0.1,
+        'lambda': 0.95,
+        'vf_clip_param': 100,
+
+        'num_workers': 3,
+        'num_cpus_per_worker': 1,
+        'num_cpus_for_driver': 1,
+        'num_envs_per_worker': 1,
+
+        'log_level': 'WARNING',
+
+        'model': {
+            'fcnet_activation': 'tanh',
+            'fcnet_hiddens': [128, 64, 32],
+            'free_log_std': False,
+            'vf_share_layers': True,
+            'use_lstm': False,
+            'state_shape': None,
+            'framestack': False,
+            'zero_mean': True,
+        },
+
+        # ==== EXPLORATION ====
+        'explore': True,
+        'exploration_config': {
+            'type': 'StochasticSampling',  # default for PPO
+        },
+
+        # ==== EVALUATION ====
+        "evaluation_num_workers": 0,
+        'evaluation_num_episodes': 2,
+        "evaluation_interval": 1,
+        "custom_eval_function": tune.function(custom_eval_function),
+        'evaluation_config': {
+            "seed": 42,
+            # IMPORTANT NOTE: For policy gradients, this might not be the optimal policy
+            'explore': False,
+            'env_config': deepcopy(sim_params),
+        },
+
+        # ==== CUSTOM METRICS ====
+        "callbacks": {
+            "on_episode_start": tune.function(on_episode_start),
+            "on_episode_step": tune.function(on_episode_step),
+            "on_episode_end": tune.function(on_episode_end),
+        },
+    }
+    # eval environment should not be random across workers
+    eval_start = 100  # random.randint(0, 3599 - 500)
+    base_config['evaluation_config']['env_config']['scenario_config']['start_end_time'] = [eval_start, eval_start + 750]
+    base_config['evaluation_config']['env_config']['scenario_config']['multi_config'] = False
+    del base_config['evaluation_config']['env_config']['attack_randomization']
+
+    ray.init(local_mode=True)
 
     base_config['num_workers'] = args.workers
     base_config['evaluation_num_episodes'] = args.eval_rounds
@@ -299,11 +345,16 @@ if __name__ == '__main__':
         'config': base_config,
         'epochs': args.epochs,
         'save_path': args.save_path,
-        'algo': args.algo
+        'algo': args.algo,
+        'unbalance': args.unbalance
     }
 
     if args.algo == 'ppo':
         
+        config = deepcopy(full_config)
+        run_hp_experiment(config, 'main')
+
+        """
         config = deepcopy(full_config)
         config['config']['env_config']['N'] = ray.tune.grid_search([0, 1, 2, 4, 8])
         run_hp_experiment(config, 'action_penalty')
@@ -335,7 +386,7 @@ if __name__ == '__main__':
         config = deepcopy(full_config)
         config['config']['lr'] = ray.tune.grid_search([2e-6, 2e-5, 2e-4, 2e-3, 2e-2])
         run_hp_experiment(config, 'lr')
-
+        """
     elif args.algo == 'appo':
         config = deepcopy(full_config)
         run_hp_experiment(config, 'appo')
