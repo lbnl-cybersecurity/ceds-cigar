e5d15d169468d30092f8c5dd6447ce0c5fc5e29b
toanngo <toanngosy@gmail.com>
2020-04-26 14:15:12 -0700

    move old code to archive

--- rl/pycigar/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Returns features of the PyCIGAR repository (e.g. version number)."""
-
-from .version import __version__ as version
-
-# flow repo version number
-__version__ = version
--- rl/result/__init__.py
+++
@@ -1,1 +0,0 @@
-"Create a placeholder for the result folder. All plots from experiments will be saved under this directory."
