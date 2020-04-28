ef570515333c596dadc329ef1d86fd15c827a07a
milesial <milesial@users.noreply.github.com>
2020-04-28 02:56:45 -0700

    Change discretize params

--- rl/pycigar/envs/wrappers/wrappers_constants.py
+++ rl/pycigar/envs/wrappers/wrappers_constants.py
@@ -10,16 +10,8 @@ ACTION_CURVE = np.array([-0.03, 0., 0., 0.03, 0.07])
 ACTION_LOWER_BOUND = 0.8
 ACTION_UPPER_BOUND = 1.1
 
-# relative single head action
-ACTION_RANGE = 0.1
-ACTION_STEP = 0.01
-DISCRETIZE_RELATIVE = int((ACTION_RANGE / ACTION_STEP)) * 2 + 1
-
 ACTION_MIN_SLOPE = 0.02  # actually the slope is stepper when value is small
 ACTION_MAX_SLOPE = 0.07
-
-ACTION_COMBINATION = [[x, y] for x in range(DISCRETIZE_RELATIVE) for y in range(DISCRETIZE_RELATIVE)]
-ACTION_MAP = {k: a for k, a in enumerate(ACTION_COMBINATION)}
 
 # number of frames to keep
 NUM_FRAMES = 10
@@ -27,6 +19,6 @@ NUM_FRAMES = 10
 
 ACTION_MAX_SLOPE = 0.051
 ACTION_MIN_SLOPE = 0.001
-ACTION_RANGE = 0.25
+ACTION_RANGE = 0.1
 ACTION_STEP = 0.01
 DISCRETIZE_RELATIVE = int((ACTION_RANGE / ACTION_STEP)) * 2 + 1
