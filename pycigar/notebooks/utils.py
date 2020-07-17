2404e5519b382df003a21de79a6787de0fa7fe3a
milesial <milesial@users.noreply.github.com>
2020-07-17 16:26:10 -0700

    Clean up eval routine and add 2 metrics

--- pycigar/notebooks/utils.py
+++ pycigar/notebooks/utils.py
@@ -1,10 +1,11 @@
 import argparse
 import json
 import math
-import os
 import shutil
-import time
 from collections import namedtuple
+from enum import Enum
+from pathlib import Path
+from typing import List
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -12,83 +13,135 @@ import pandas as pd
 import ray
 from pycigar.utils.logging import logger
 from pycigar.utils.output import plot_new
-from ray.rllib.evaluation.metrics import collect_episodes, summarize_episodes
+from ray.rllib.agents import Trainer
 from ray.rllib.agents.callbacks import DefaultCallbacks
 from ray.rllib.env.base_env import _MultiAgentEnvToBaseEnv
-from pathlib import Path
+from ray.rllib.evaluation.metrics import collect_episodes, summarize_episodes
+from ray.rllib.evaluation.rollout_metrics import RolloutMetrics
+from ray.rllib.evaluation.worker_set import WorkerSet
 
-def custom_eval_function(trainer, eval_workers):
-    def set_hack(env, hack):
-        for node in env.k.sim_params['scenario_config']['nodes']:
-            for d in node['devices']:
-                d['hack'][1] = hack
 
+class EvalMetric(Enum):
+    REWARD_SUM = 1
+    REWARD_MIN = 2
+    REWARD_MAX = 3
+    NEG_Y_U_SUM = 4
+    NEG_Y_U_MAX = 5
+    NEG_Y_U_MIN = 6
+
+
+def _set_env_hack(env, hack_magnitude: float):
+    # set the hack magnitude for a given env
+    # Warning: this may not work when called more than 1 time on a worker
+    for node in env.k.sim_params["scenario_config"]["nodes"]:
+        for d in node["devices"]:
+            d["hack"][1] = hack_magnitude
+
+
+def local_worker_single_hack_eval(eval_workers: WorkerSet, num_episodes: int, hack_magnitude=0.45) -> np.ndarray:
+    """One one worker (the local one), do `evaluation_num_episodes` runs for the same hack magnitude"""
+    hack_mags = np.repeat([hack_magnitude], num_episodes)
+    eval_workers.local_worker().foreach_env(lambda env: _set_env_hack(env, hack_magnitude))
+
+    for _ in range(num_episodes):
+        eval_workers.local_worker().sample()
+
+    return hack_mags
+
+
+def remote_workers_multi_hacks_eval(eval_workers: WorkerSet, num_episodes: int, min_hack=0.1,
+                                    max_hack=0.5) -> np.ndarray:
+    """The remote workers do n_workers runs with different hacks. This can be repeated if num_episodes > n_workers"""
+
+    workers = eval_workers.remote_workers()
+    n_workers = len(workers)
+
+    # number of full rounds that will be run (num_rounds * n_workers will be > num_episodes
+    # if num_episodes / n_workers is fractional)
+    num_rounds = int(math.ceil(num_episodes / n_workers))
+
+    # here we use n_workers and not num_rounds * n_workers because we can't seem to change the
+    # hack more than once per worker (to be investigated)
+    hack_mags = np.repeat(np.linspace(min_hack, max_hack, n_workers), num_episodes)
+
+    # zip will ignore the repeated hacks because len(workers) <= len(hack_mags)
+    for w, h in zip(workers, hack_mags):
+        w.foreach_env.remote(lambda env: _set_env_hack(env, h))
+
+    for i in range(num_rounds):
+        ray.get([w.sample.remote() for w in eval_workers.remote_workers()])
+
+    return hack_mags
+
+
+def custom_eval_function(trainer: Trainer, eval_workers: WorkerSet):
+    num_episodes = trainer.config["evaluation_num_episodes"]
     if trainer.config["evaluation_num_workers"] == 0:
-        hacks = [0.45] * trainer.config["evaluation_num_episodes"]
-        eval_workers.local_worker().foreach_env(lambda env: set_hack(env, hacks[0]))
-
-        for _ in range(trainer.config["evaluation_num_episodes"]):
-            eval_workers.local_worker().sample()
+        hack_mags = local_worker_single_hack_eval(eval_workers, num_episodes, hack_magnitude=0.45)
     else:
-        num_rounds = int(
-            math.ceil(trainer.config["evaluation_num_episodes"] / trainer.config["evaluation_num_workers"])
-        )
-        workers = eval_workers.remote_workers()
-        hacks = np.linspace(0.1, 0.5, trainer.config["evaluation_num_workers"]).tolist() * trainer.config["evaluation_num_episodes"]
-
-        for w, h in zip(workers, hacks):
-            w.foreach_env.remote(lambda env: set_hack(env, h))
-
-        for i in range(num_rounds):
-            ray.get([w.sample.remote() for w in eval_workers.remote_workers()])
+        hack_mags = local_worker_single_hack_eval(eval_workers, num_episodes)
 
     episodes, _ = collect_episodes(eval_workers.local_worker(), eval_workers.remote_workers())
-    metrics = summarize_episodes(episodes)
 
-    for ep, hack in zip(episodes[:max(1, trainer.config["evaluation_num_workers"])+1], hacks):
-        dir = Path(trainer.global_vars['reporter_dir']) / f'hack_{hack}'
+    # save figures (each hack magnitude has its directory)
+    # we don't want to save dupliates so we remove hack_mags duplicates
+    # the order of the episodes is the same as the order of hack_mags so this is fine
+    for ep, hack in zip(episodes, set(hack_mags)):
+        dir = Path(trainer.global_vars["reporter_dir"]) / f"hack_{hack:.2f}"
         dir.mkdir(exist_ok=True, parents=True)
         f = plot_new(
-            ep.hist_data['logger']['log_dict'],
-            ep.hist_data['logger']['custom_metrics'],
+            ep.hist_data["logger"]["log_dict"],
+            ep.hist_data["logger"]["custom_metrics"],
             trainer.iteration,
-            trainer.global_vars.get('unbalance', False),
-            trainer.global_vars.get('multiagent', False),
+            trainer.global_vars.get("unbalance", False),
+            trainer.global_vars.get("multiagent", False),
         )
-        f.savefig(str(dir / f'eval-epoch-{trainer.iteration}-hack-{hack:.2f}.png'),
-                bbox_inches='tight')
+        f.savefig(str(dir / f'eval-epoch-{trainer.iteration}-hack-{hack:.2f}.png'), bbox_inches='tight')
         plt.close(f)
 
-    for m in ["reward_sum", "reward_min", "neg_y_u_sum", "neg_y_u_max"]:
-        save_best_policy(trainer, episodes, m)
+    # save best policy w.r.t. multiple eval objectives
+    for metric in EvalMetric:
+        save_best_policy(trainer, episodes, metric)
+
+    metrics = summarize_episodes(episodes)
     return metrics
 
 
-def get_metric_from_episodes(trainer, episodes, metric):
-    assert metric in ["reward_sum", "reward_min", "neg_y_u_sum", "neg_y_u_max"]
-
+def get_metric_from_episodes(trainer: Trainer, episodes: List[RolloutMetrics], metric: EvalMetric) -> float:
     y_u = 'u' if trainer.global_vars.get('unbalance', False) else 'y'
     log_dicts = [ep.hist_data['logger']['log_dict'] for ep in episodes]
-    sum_us = [np.mean([sum(log_dict[k][y_u]) for k in log_dict if y_u in log_dict[k]]) for log_dict in log_dicts]
 
-    if metric == "reward_sum":
-        return np.array([ep.episode_reward for ep in episodes]).sum()
-    elif metric == "reward_min":
-        return np.array([ep.episode_reward for ep in episodes]).min()
-    elif metric == "neg_y_u_sum":
-        return sum(sum_us)
-    elif metric == "neg_y_u_max":
-        return max(sum_us)
+    # mean of [the sum of the y or u over the simulation] over all inverters
+    sum_yu = [
+        np.mean([
+            sum(log_dict[k][y_u])
+            for k in log_dict if y_u in log_dict[k]])
+        for log_dict in log_dicts]
+
+    if metric == EvalMetric.REWARD_SUM:
+        return sum([ep.episode_reward for ep in episodes])
+    elif metric == EvalMetric.REWARD_MIN:
+        return min([ep.episode_reward for ep in episodes])
+    elif metric == EvalMetric.REWARD_MAX:
+        return max([ep.episode_reward for ep in episodes])
+    elif metric == EvalMetric.NEG_Y_U_SUM:
+        return -sum(sum_yu)
+    elif metric == EvalMetric.NEG_Y_U_MAX:
+        return -max(sum_yu)
+    elif metric == EvalMetric.NEG_Y_U_MIN:
+        return -min(sum_yu)
+    else:
+        raise NotImplementedError('Unsupported metric ' + metric)
 
 
-def save_best_policy(trainer, episodes, metric_name):
-    metric = get_metric_from_episodes(trainer, episodes, metric_name)
+def save_best_policy(trainer: Trainer, episodes: List[RolloutMetrics], metric: EvalMetric):
+    metric_value = get_metric_from_episodes(trainer, episodes, metric)
     if 'best_eval' not in trainer.global_vars:
         trainer.global_vars['best_eval'] = {}
 
-    if trainer.global_vars['best_eval'].get(metric_name, -np.Inf) < metric:
-        trainer.global_vars['best_eval'][metric_name] = metric
-        best_dir = Path(trainer.global_vars['reporter_dir']) / 'best' / metric_name
+    if trainer.global_vars['best_eval'].get(metric.name, -np.Inf) < metric_value:
+        trainer.global_vars['best_eval'][metric.name] = metric_value
+        best_dir = Path(trainer.global_vars['reporter_dir']) / 'best' / metric.name
         best_dir.mkdir(exist_ok=True, parents=True)
 
         # save policy
@@ -96,8 +149,10 @@ def save_best_policy(trainer, episodes, metric_name):
             if path.exists():
                 shutil.rmtree(str(path), ignore_errors=True)
 
+            # single policy
             if trainer.get_policy() is not None:
                 trainer.get_policy().export_model(str(path))
+            # multiple policies
             else:
                 for k, p in trainer.optimizer.policies.items():
                     p_path = path / k
@@ -105,7 +160,7 @@ def save_best_policy(trainer, episodes, metric_name):
 
         try:
             save_policy_to_path(best_dir / 'policy')
-        except Exception: # hack for lawrencium
+        except Exception:  # hack for lawrencium because it can't seem to fully delete the old dir
             save_policy_to_path(best_dir / f'policy_{trainer.iteration}')
 
         # save plots
@@ -119,6 +174,8 @@ def save_best_policy(trainer, episodes, metric_name):
             f.savefig(str(best_dir / f'eval_{i}.png'))
             plt.close(f)
 
+        # CSVs for the plots in the paper
+        # only for one arbitrary episode
         ep = episodes[-1]
         # save CSV
         k = [k for k in data if k.startswith('inverter_s701')][0]
@@ -144,7 +201,7 @@ def save_best_policy(trainer, episodes, metric_name):
         # save info
         start = ep.custom_metrics["hack_start"]
         end = ep.custom_metrics["hack_end"]
-        info = {'epoch': trainer.iteration, 'hack_start': start, 'hack_end': end, 'metric': metric}
+        info = {'epoch': trainer.iteration, 'hack_start': start, 'hack_end': end, 'metric': metric_value}
         with open(str(best_dir / 'info.json'), 'w', encoding='utf-8') as f:
             json.dump(info, f, ensure_ascii=False, indent=4)
 
