95dc58eb56280e85839ae0c65ac588f6486befa0
milesial <milesial@users.noreply.github.com>
2020-07-21 08:52:20 -0700

    Cleanup for multiagent script

--- pycigar/notebooks/unbalance/unbalance_multiagent.py
+++ pycigar/notebooks/unbalance/unbalance_multiagent.py
@@ -1,19 +1,21 @@
-from ray import tune
+import argparse
 import os
 import pickle
-from tqdm import tqdm
+from copy import deepcopy
+from pathlib import Path
+
+import numpy as np
+import pycigar
+import ray
+from pycigar.notebooks.utils import add_common_args, get_base_config
+from pycigar.utils.input_parser import input_parser
+from ray import tune
 from ray.rllib.agents.ppo import PPOTrainer
 from ray.rllib.agents.ppo.ppo_tf_policy import PPOTFPolicy
-from pycigar.notebooks.utils import custom_eval_function, CustomCallbacks, add_common_args
-import argparse
-from copy import deepcopy
-from pycigar.utils.registry import make_create_env
-from ray.tune.registry import register_env
-from pycigar.utils.input_parser import input_parser
 from ray.rllib.policy.policy import Policy
-import pycigar
-import numpy as np
-import ray
+from tqdm import tqdm
+
+from notebooks.unbalance.unbalance import set_unbalance_attack, adjust_default_curves
 
 
 def parse_cli_args():
@@ -31,15 +33,15 @@ def run_train(config, reporter):
     trainer.global_vars['unbalance'] = True  # for plots
     trainer.global_vars['multiagent'] = True  # for plots
 
-
     for _ in tqdm(range(config['epochs'])):
         results = trainer.train()
-#        del results['hist_stats']['logger']  # don't send to tensorboard
- #       if 'evaluation' in results:
-  #          del results['evaluation']['hist_stats']['logger']
+        #        del results['hist_stats']['logger']  # don't send to tensorboard
+        #       if 'evaluation' in results:
+        #          del results['evaluation']['hist_stats']['logger']
         reporter(**results)
 
     trainer.stop()
+
 
 def run_experiment(full_config, name):
     res = tune.run(
@@ -80,7 +82,6 @@ class DoNothingPolicy(Policy):
                         info_batch=None,
                         episodes=None,
                         **kwargs):
-
         return [0 for x in obs_batch], [], {}
 
     def learn_on_batch(self, samples):
@@ -95,118 +96,50 @@ class DoNothingPolicy(Policy):
 
 if __name__ == '__main__':
     args = parse_cli_args()
+    feeder_path = Path(pycigar.DATA_DIR) / 'ieee37busdata_regulator_attack'
 
-    pycigar_params = {
-        'exp_tag': 'cooperative_multiagent_ppo',
-        'env_name': 'PhaseSpecificContinuousMultiEnv',
-        'simulator': 'opendss',
-    }
+    sim_params = input_parser(misc_inputs_path=str(feeder_path / 'misc_inputs.csv'),
+                              dss_path=str(feeder_path / 'ieee37.dss'),
+                              load_solar_path=str(feeder_path / 'load_solar_data.csv'),
+                              breakpoints_path=str(feeder_path / 'breakpoints.csv'))
+    base_config, create_env = \
+        get_base_config(env_name='PhaseSpecificContinuousMultiEnv',
+                        cli_args=args,
+                        sim_params=sim_params)
 
-    create_env, env_name = make_create_env(pycigar_params, version=0)
-    register_env(env_name, create_env)
+    set_unbalance_attack(base_config)
+    adjust_default_curves(base_config)
 
-
-    misc_inputs_path = pycigar.DATA_DIR + "/ieee37busdata_regulator_attack/misc_inputs.csv"
-    dss_path = pycigar.DATA_DIR + "/ieee37busdata_regulator_attack/ieee37.dss"
-    load_solar_path = pycigar.DATA_DIR + "/ieee37busdata_regulator_attack/load_solar_data.csv"
-    breakpoints_path = pycigar.DATA_DIR + "/ieee37busdata_regulator_attack/breakpoints.csv"
-
-    sim_params = input_parser(misc_inputs_path, dss_path, load_solar_path, breakpoints_path)
     test_env = create_env(sim_params)
     obs_space = test_env.observation_space
     act_space = test_env.action_space
-
     model = {
-            'fcnet_activation': 'tanh',
-            'fcnet_hiddens': [64, 64, 32],
-            'free_log_std': False,
-            'vf_share_layers': True,
-            'use_lstm': False,
-            'state_shape': None,
-            'framestack': False,
-            'zero_mean': True,
-        }
-    base_config = {
-        "env": env_name,
-        "gamma": 0.5,
-        'lr': 2e-3,
-        'env_config': deepcopy(sim_params),
-        'rollout_fragment_length': 35,
-        'train_batch_size': max(500, 35 * args.workers),
-        'clip_param': 0.15,
-        'lambda': 0.95,
-        'vf_clip_param': 10000,
-        'num_workers': args.workers,
-        'num_cpus_per_worker': 1,
-        'num_cpus_for_driver': 1,
-        'num_envs_per_worker': 1,
-        'log_level': 'WARNING',
-        # ==== EXPLORATION ====
-        'explore': True,
-        'exploration_config': {'type': 'StochasticSampling',},  # default for PPO
-        # ==== EVALUATION ====
-        "evaluation_num_workers": 0 if args.local_mode else 5,
-        'evaluation_num_episodes': args.eval_rounds,
-        "evaluation_interval": args.eval_interval,
-        "custom_eval_function": custom_eval_function,
-        'evaluation_config': {
-            "seed": 42,
-            # IMPORTANT NOTE: For policy gradients, this might not be the optimal policy
-            'explore': False,
-            'env_config': deepcopy(sim_params),
-        },
-        # ==== CUSTOM METRICS ====
-        "callbacks": CustomCallbacks,
-        # ==== MULTI AGENT ====
-        "multiagent": {
-            "policies_to_train": ["phase_a", "phase_b", "phase_c"],
-            "policies": {
-                "phase_a": (PPOTFPolicy, obs_space, act_space, model),
-                "phase_b": (PPOTFPolicy, obs_space, act_space, model),
-                "phase_c": (PPOTFPolicy, obs_space, act_space, model),
-                "do_nothing": (DoNothingPolicy, obs_space, act_space, {})
-            },
-            "policy_mapping_fn": policy_mapping_fn
-        }
+        'fcnet_activation': 'tanh',
+        'fcnet_hiddens': [64, 64, 32],
+        'free_log_std': False,
+        'vf_share_layers': True,
+        'use_lstm': False,
+        'state_shape': None,
+        'framestack': False,
+        'zero_mean': True,
     }
-
-    # eval environment should not be random across workers
-    eval_start = 100  # random.randint(0, 3599 - 500)
-    base_config['evaluation_config']['env_config']['scenario_config']['start_end_time'] = [eval_start, eval_start + 750]
-    base_config['evaluation_config']['env_config']['scenario_config']['multi_config'] = False
-    del base_config['evaluation_config']['env_config']['attack_randomization']
-    del base_config['env_config']['attack_randomization']
-
-    for node in base_config['env_config']['scenario_config']['nodes']:
-        for d in node['devices']:
-            d['adversary_controller'] = 'unbalanced_fixed_controller'
-    for node in base_config['evaluation_config']['env_config']['scenario_config']['nodes']:
-        for d in node['devices']:
-            d['adversary_controller'] = 'unbalanced_fixed_controller'
-
-    for node in (
-            base_config['env_config']['scenario_config']['nodes']
-            + base_config['evaluation_config']['env_config']['scenario_config']['nodes']
-    ):
-        for d in node['devices']:
-            name = d['name']
-            c = np.array(d['custom_configs']['default_control_setting'])
-            #            found by training with no attack
-            if name.endswith('a'):
-                c = c - 0.02
-            elif name.endswith('b'):
-                c = c + 0.02
-            elif name.endswith('c'):
-                c = c - 0.01
-            d['custom_configs']['default_control_setting'] = c
+    base_config['multiagent'] = {
+        "policies_to_train": ["phase_a", "phase_b", "phase_c"],
+        "policies": {
+            "phase_a": (PPOTFPolicy, obs_space, act_space, model),
+            "phase_b": (PPOTFPolicy, obs_space, act_space, model),
+            "phase_c": (PPOTFPolicy, obs_space, act_space, model),
+            "do_nothing": (DoNothingPolicy, obs_space, act_space, {})
+        },
+        "policy_mapping_fn": policy_mapping_fn
+    }
 
     if args.redis_pwd:
         ray.init(address='auto', redis_password=args.redis_pwd)
+        print("Nodes in the Ray cluster:")
+        print(ray.nodes())
     else:
         ray.init(local_mode=args.local_mode)
-
-    print("Nodes in the Ray cluster:")
-    print(ray.nodes())
 
     full_config = {
         'config': base_config,
