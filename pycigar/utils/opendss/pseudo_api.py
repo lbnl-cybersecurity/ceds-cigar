c1f9bd99632bb74ec97492dcbfe7182ce214d0d2
toanngo <toanngosy@gmail.com>
2020-10-06 09:44:06 -0700

    update api

--- pycigar/utils/opendss/pseudo_api.py
+++ pycigar/utils/opendss/pseudo_api.py
@@ -2,6 +2,8 @@
 import opendssdirect as dss
 import numpy as np
 import warnings
+from pycigar.utils.logging import logger
+import time
 
 class PyCIGAROpenDSSAPI(object):
     """An API used to interact with OpenDSS via a TCP connection."""
@@ -23,6 +25,18 @@ class PyCIGAROpenDSSAPI(object):
     def simulation_command(self, command):
         """Run an custom command on simulator."""
         dss.run_command(command)
+        self.all_bus_name = dss.Circuit.AllBusNames()
+
+        self.offsets = {}
+        for k, v in enumerate(dss.Circuit.AllNodeNames()):
+            self.offsets[v] = k
+        self.loads = {}
+        for load in self.get_node_ids():
+            dss.Loads.Name(load)
+            bus_phase = dss.CktElement.BusNames()[0].split('.')
+            if len(bus_phase) == 1:
+                bus_phase.extend(['1','2','3'])
+            self.loads[load] = [['.'.join([bus_phase[0], i]) for i in bus_phase[1:] if i != '0'], dss.CktElement.NumPhases()]
 
     def set_solution_mode(self, value):
         """Set solution mode on simulator."""
@@ -60,40 +74,25 @@ class PyCIGAROpenDSSAPI(object):
         nodes = dss.Loads.AllNames()
         return nodes
 
-    def get_node_voltage(self, node_id):
-        """Get node voltage given node id."""
-        dss.Loads.Name(node_id)
-        _voltage = dss.CktElement.VoltagesMagAng()
-        # print(voltage, node_id, dss.CktElement.NumPhases())
-        if len(_voltage) == 6 or len(_voltage) == 8:
-            voltage = (_voltage[0] + _voltage[2] + _voltage[4]) / (dss.CktElement.NumPhases() * (dss.Loads.kV() * 1000 / (3**0.5)))
-        else:
-            if node_id[-1] == 'a':
-                voltage = (_voltage[0]) / (dss.CktElement.NumPhases() * (dss.Loads.kV() * 1000 / (3 ** 0.5)))
-            elif node_id[-1] == 'b':
-                voltage = (_voltage[0]) / (dss.CktElement.NumPhases() * (dss.Loads.kV() * 1000 / (3 ** 0.5)))
-            else:
-                voltage = (_voltage[0]) / (dss.CktElement.NumPhases() * (dss.Loads.kV() * 1000 / (3 ** 0.5)))
+    def update_all_bus_voltages(self):
+        self.puvoltage = dss.Circuit.AllBusMagPu() #work around this
 
-        # get the pu information directly
-        if np.isnan(voltage) or np.isinf(voltage):
-            print('NumPhases', dss.CktElement.NumPhases())
-            print('kV', dss.Loads.kV())
-            print('voltage', _voltage)
-            raise ValueError(
-                'Voltage Output {} from OpenDSS for Load {} at Bus {} is not appropriate.'.format(
-                    np.mean(voltage), node_id, dss.CktElement.BusNames()[0]
-                )
-            )
-        else:
-            output = np.mean(voltage)
-        return output
+    def get_node_voltage(self, node_id):
+        puvoltage = 0 # get rid of this
+        for phase in range(self.loads[node_id][1]):
+            puvoltage += self.puvoltage[self.offsets[self.loads[node_id][0][phase]]]
+        puvoltage /= self.loads[node_id][1]
+        return puvoltage
 
     def get_total_power(self):
-        return np.array(dss.Circuit.TotalPower())
+        start_time = time.time()
+        result = np.array(dss.Circuit.TotalPower())
+        if 'opendss_time' not in logger().custom_metrics:
+            logger().custom_metrics['opendss_time'] = 0
+        logger().custom_metrics['opendss_time'] += time.time() - start_time
 
     def get_losses(self):
-        return np.array(dss.Circuit.Losses())
+        result = np.array(dss.Circuit.Losses())
 
     def set_node_kw(self, node_id, value):
         """Set node kW."""
@@ -111,7 +110,8 @@ class PyCIGAROpenDSSAPI(object):
 
     # ######################## REGULATOR ############################
     def get_all_regulator_names(self):
-        return dss.RegControls.AllNames()
+        result = dss.RegControls.AllNames()
+
 
     def set_regulator_property(self, reg_id, prop):
         dss.RegControls.Name(reg_id)
@@ -130,7 +130,6 @@ class PyCIGAROpenDSSAPI(object):
                     dss.RegControls.Delay(v)
                 else:
                     print('Regulator Parameters unknown by PyCIGAR. Checkout pycigar/utils/opendss/pseudo_api.py')
-        pass
 
     def get_regulator_tap(self, reg_id):
         dss.RegControls.Name(reg_id)
@@ -145,19 +144,42 @@ class PyCIGAROpenDSSAPI(object):
         return dss.RegControls.ForwardVreg()
 
     def get_substation_top_voltage(self):
-        dss.Bus.Name()
-        voltage = dss.Bus.VMagAngle()
-        voltage = (voltage[0] + voltage[2] + voltage[4]) / (
-            dss.CktElement.NumPhases() * (dss.Loads.kV() * 1000 / (3 ** 0.5))
-        )
+        sourcebus = self.all_bus_name[0]
+        num_phases = 0
+        voltage = 0
+        for i in range(3):
+            num_phases += 1
+            voltage += self.puvoltage[self.offsets['{}.{}'.format(sourcebus, i+1)]]
+        voltage /= num_phases
         return voltage
 
     def get_substation_bottom_voltage(self):
-        dss.Loads.Name('S701a')
-        voltage_a = dss.CktElement.VoltagesMagAng()
-        dss.Loads.Name('S701b')
-        voltage_b = dss.CktElement.VoltagesMagAng()
-        dss.Loads.Name('S701c')
-        voltage_c = dss.CktElement.VoltagesMagAng()
-        voltage = (voltage_a[0] + voltage_b[0] + voltage_c[0]) / (3 * (dss.Loads.kV() * 1000 / (3 ** 0.5)))
+        voltage_a = self.get_node_voltage('s701a')
+        voltage_b = self.get_node_voltage('s701b')
+        voltage_c = self.get_node_voltage('s701c')
+        voltage = (voltage_a + voltage_b + voltage_c) / 3
         return voltage
+
+
+    def get_worst_u_node(self):
+        buses = dss.Circuit.AllBusNames()
+        u_all = []
+        v_all = {}
+        u_all_real = {}
+        u_worst = 0
+        for bus in self.all_bus_name:
+            va = self.puvoltage[self.offsets['{}.{}'.format(bus, 1)]]
+            vb = self.puvoltage[self.offsets['{}.{}'.format(bus, 2)]]
+            vc = self.puvoltage[self.offsets['{}.{}'.format(bus, 3)]]
+
+            v_all[bus] = [va, vb, vc]
+            mean = (va + vb + vc) / 3
+            max_diff = max(abs(va - mean), abs(vb - mean), abs(vc - mean))
+            u = max_diff / mean
+            if u > u_worst:
+                u_worst = u
+                v_worst = [va, vb, vc]
+            u_all.append(u)
+            u_all_real[bus] = u
+
+        return u_worst, v_worst, np.mean(u_all), np.std(u_all), v_all, u_all_real
