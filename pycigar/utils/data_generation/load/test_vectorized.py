31d09cf76257e8d38fe260d317bf511a315857a0
toanngo <toanngosy@gmail.com>
2021-05-17 22:33:15 -0700

    remove heavy files

--- pycigar/utils/data_generation/load/test_vectorized.py
+++ pycigar/utils/data_generation/load/test_vectorized.py
@@ -22,12 +22,24 @@
 # print('ahihi')
 
 
+# from init import *
+# from src.process_data import read_initial_data
+# data = "C:\\Users\\kathl\Desktop\\Github\\ceds-cigar\\pycigar\\utils\\data_generation\\load\\data"
+# data = [data + '\\7_MWp_P.csv', data + '\\10_MWp_P.csv', data + '\\12_MWp_P.csv', data + '\\19_MWp_P.csv']
+
+# dss_kw = [0.14, 0.16, 0.18, 0.07, 0.09] #assumes input in kw
+# load_gen = LoadGenerator(data, dss_kw) 
+
+# a = load_gen.generate_load([7, 9, 10, 12],[ .14, .16] )
+
 from init import *
 from src.process_data import read_initial_data
-data = "C:\\Users\\kathl\Desktop\\Github\\ceds-cigar\\pycigar\\utils\\data_generation\\load\\data"
-data = [data + '\\7_MWp_P.csv', data + '\\10_MWp_P.csv', data + '\\12_MWp_P.csv', data + '\\19_MWp_P.csv']
+#data = "C:\\Users\\kathl\Desktop\\Github\\ceds-cigar\\pycigar\\utils\\data_generation\\load\\data"
+#data = [data + '\\7_MWp_P.csv', data + '\\10_MWp_P.csv', data + '\\12_MWp_P.csv', data + '\\19_MWp_P.csv']
+data = '/home/toanngo/Documents/GitHub/ceds-cigar/pycigar/utils/data_generation/load/data'
+data = [data + '/7_MWp_P.csv', data + '/10_MWp_P.csv', data + '/12_MWp_P.csv', data + '/19_MWp_P.csv']
 
 dss_kw = [0.14, 0.16, 0.18, 0.07, 0.09] #assumes input in kw
-load_gen = LoadGenerator(data, dss_kw) 
+load_gen = LoadGenerator(data, dss_kw)
 
-a = load_gen.generate_load([7, 9, 10, 12],[ .14, .16] )
+out = load_gen.generate_load(dss_kw)
