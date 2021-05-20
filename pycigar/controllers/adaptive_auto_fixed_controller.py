ba9e1935424ffce9e35fb7ae1273b8a635b6be6d
Sy-Toan Ngo <sytoanngo@n0000.scs00>
2021-05-20 15:53:28 -0700

    add cooldown timer

--- pycigar/controllers/adaptive_auto_fixed_controller.py
+++ pycigar/controllers/adaptive_auto_fixed_controller.py
@@ -22,13 +22,18 @@ class AdaptiveAutoFixedController(BaseController):
         self.average_span = 10
 
         self.action = None
+        self.countdown_timer = 0
+        self.countdown = 15
 
     def get_action(self, env):
         """See parent class."""
         # nothing to do here, the setting in the device is as default
         index = env.k.device.vectorized_pv_inverter_device.list_device.index(self.device_id)
-        if env.k.device.vectorized_pv_inverter_device.y[index] < 0.02:
+        if env.k.device.vectorized_pv_inverter_device.y[index] < 0.02 and self.countdown_timer >= self.countdown:
             self.trigger = False
+            self.countdown_timer = 0
+        
+        self.countdown_timer += 1
 
         if self.trigger is False:
             node_id = env.k.device.devices[self.device_id]['node_id']
